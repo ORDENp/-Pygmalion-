c8f3de3e828d4d7f4096fa0f56a776b265b309c8
ORDENp <tvoy1000@gmail.com>
2026-01-18 16:51:31 +0300

    fix: deploy v0.2.2 correct paths.

--- mvp/app/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-// vite.config.js
-export default {
-  base: '/-Pygmalion-/mvp/app/',
-  // остальная конфигурация...
-}
