4aa5344f0143c4f474d7c6b683368fccc4b1f892
OP𝕯EH 𝕯AP <tvoy1000@gmail.com>
2026-04-15 04:26:57 +0300

    Update logic.js
    olegDal.tmp

--- sandbox-v0.3.25/logic.js
+++ sandbox-v0.3.25/logic.js
@@ -2663,7 +2663,7 @@ function initMusicToggle() {
     if (!musicBtn) return;
 
     // Путь к музыке в репозитории GitHub Pages
-    const audio = new Audio('/-Pygmalion-/assets/audio/olegDal.mp3');
+    const audio = new Audio('/-Pygmalion-/assets/audio/olegDal.tmp');
     audio.loop = true;
     audio.volume = 0.5;
 
